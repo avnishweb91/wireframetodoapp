b200411bccda9822f697a0df09444c1341243227
amacshri <avnishweb91@gamil.com>
2024-01-18 15:21:34 +0530

    redploy

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://localhost:8080/todolist/api/v1/user";
+const USER_API_BASE_URL = "http://52.66.203.162:8080/todolist/api/v1/user";
 
 class UserService {
   createTask(task) {
